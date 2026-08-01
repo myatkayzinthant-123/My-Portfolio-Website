635c1b603cbea2b16825f9c2a87dc8daebc1960f
myatkayzinthant-123 <kkzt31583@gmail.com>
2026-08-01 20:30:04 +0630

    migrate to custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-//configured to fix github routing page
-const base = '/My-Portfolio-Website'
+//migrate to custom domain
+const base = '/'
 
 // https://vite.dev/config/
 export default defineConfig({
