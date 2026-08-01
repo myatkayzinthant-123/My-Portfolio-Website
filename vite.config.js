faef7ac6157b3c1e239738bd30ac7d10d2df1356
myatkayzinthant-123 <kkzt31583@gmail.com>
2026-08-01 19:41:25 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+//configured to fix github routing page
+const base = '/My-Portfolio-Website'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  base
 })
