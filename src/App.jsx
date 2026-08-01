734b2e7fe413a624c3779bdec97711becf282a69
myatkayzinthant-123 <kkzt31583@gmail.com>
2026-08-01 20:30:59 +0630

    migrate to custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import ProjectPage from "./pages/ProjectPage";
-import ProjectDetail from "./pages/ProjectDetail";
-
-function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>
+  import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter } from "react-router-dom";
+  import Home from "./pages/Home";
+  import ProjectPage from "./pages/ProjectPage";
+  import ProjectDetail from "./pages/ProjectDetail";
+  
+  function App() {
+  
+    const router = createBrowserRouter([
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+      path: "/projects",
+      element: <ProjectPage/>,
     },
+  
+  
     {
-    path: "/projects",
-    element: <ProjectPage/>,
-  },
-
-
-  {
-    path: "/projects/:id",
-    element: <ProjectDetail/>,
-  },
-  ],
-    {
-      basename: "/My-Portfolio-Website",
-    }
-)
-
-  return (
-    <RouterProvider router={router}/>
+      path: "/projects/:id",
+      element: <ProjectDetail/>,
+    },
+    ],
+      {
+        basename: "/",
+      }
   )
-
-}
-export default App;
+  
+    return (
+      <RouterProvider router={router}/>
+    )
+  
+  }
+  export default App;
